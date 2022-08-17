a13184a39875543ec3b6c159415a30001be43f05
dougMR <1236472+dougMR@users.noreply.github.com>
2022-08-17 17:56:25 -0400

    changed APIUrl to change from localhost to heroku

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,3 +1,6 @@
-// const APIUrl = "http://localhost:3001";
-const APIUrl = "https://dougmr-blog-backend.herokuapp.com/";
+let APIUrl = "http://localhost:3001";
+if(window.location.host.indexOf('.herokuapp.com') !== -1){
+    APIUrl = "https://dougmr-blog-backend.herokuapp.com/";
+}
+
 export default APIUrl;
