fc7fde5959e4ab7c8d43a2fe713c0a74ec41af3d
dougMR <1236472+dougMR@users.noreply.github.com>
2022-08-17 17:58:11 -0400

    added comment

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,4 +1,5 @@
 let APIUrl = "http://localhost:3001";
+// check if we're running on heroku
 if(window.location.host.indexOf('.herokuapp.com') !== -1){
     APIUrl = "https://dougmr-blog-backend.herokuapp.com/";
 }
