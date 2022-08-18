7005b45f245f13cc8019aa9b75491b29c52d160e
dougMR <1236472+dougMR@users.noreply.github.com>
2022-08-17 20:04:53 -0400

    removed trailing / from heroku APIUrl

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,7 +1,7 @@
 let APIUrl = "http://localhost:3001";
 // check if we're running on heroku
 if(window.location.host.indexOf('.herokuapp.com') !== -1){
-    APIUrl = "https://dougmr-blog-backend.herokuapp.com/";
+    APIUrl = "https://dougmr-blog-backend.herokuapp.com";
 }
 
 export default APIUrl;
